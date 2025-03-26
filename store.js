ff0c1e146ae2e8d9f11c0470157d3da2103862f4
rayene44 <khalsirayene12@gmail.com>
2025-03-26 23:23:22 +0100

    Update store.js

--- store.js
+++ store.js
@@ -1,5 +1,6 @@
 function list1(){
 document.getElementById("list1").style.display="flex"
+ document.getElementById("list3").style.display="none"
 }
 function annuler(){
 document.getElementById("list1").style.display="none"}
@@ -7,4 +8,5 @@ function annuler2(){
     document.getElementById("list3").style.display="none"}
     function list3(){
         document.getElementById("list3").style.display="flex"
+        document.getElementById("list1").style.display="none"
         }
